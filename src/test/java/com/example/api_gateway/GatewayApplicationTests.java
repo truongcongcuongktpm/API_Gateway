962e58f37f8217c6babebad3ad84369d7614ce20
Cuong <truongcongcuong0302@gmail.com>
2021-10-07 22:47:09 +0700

    Initial commit

---
+++ src/test/java/com/example/api_gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
